9494b737abf8a41a751488e2249242e8df060fc3
smartwalle <smartwalle@gmail.com>
2023-03-24 12:05:32 +0800

    删除不必要的函数

--- digest.go
+++ digest.go
@@ -5,7 +5,6 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
-	"encoding/hex"
 )
 
 func MD5(value []byte) []byte {
@@ -14,18 +13,10 @@ func MD5(value []byte) []byte {
 	return m.Sum(nil)
 }
 
-func MD5String(value string) string {
-	return hex.EncodeToString(MD5([]byte(value)))
-}
-
 func SHA1(value []byte) []byte {
 	var s = sha1.New()
 	s.Write(value)
 	return s.Sum(nil)
-}
-
-func SHA1String(value string) string {
-	return hex.EncodeToString(SHA1([]byte(value)))
 }
 
 func SHA256(value []byte) []byte {
@@ -34,16 +25,8 @@ func SHA256(value []byte) []byte {
 	return s.Sum(nil)
 }
 
-func SHA256String(value string) string {
-	return hex.EncodeToString(SHA256([]byte(value)))
-}
-
 func SHA512(value []byte) []byte {
 	var s = sha512.New()
 	s.Write(value)
 	return s.Sum(nil)
-}
-
-func SHA512String(value string) string {
-	return hex.EncodeToString(SHA512([]byte(value)))
 }
--- digest_test.go
+++ digest_test.go
@@ -1,1 +1,17 @@
 package ncrypto_test
+
+import (
+	"testing"
+)
+
+func TestMD5(t *testing.T) {
+}
+
+func TestSHA1(t *testing.T) {
+}
+
+func TestSHA256(t *testing.T) {
+}
+
+func TestSHA512(t *testing.T) {
+}
